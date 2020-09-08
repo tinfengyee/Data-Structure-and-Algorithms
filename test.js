9d02f1d37732f269198c7ac229fbf88e5828b649
lih <v_buglh@outlook.com>
2020-09-08 17:59:54 +0800

    update insertion-sort

--- test.js
+++ test.js
@@ -1,3 +1,19 @@
-const { createRandomArray } = require('./util')
+const { createArray, swap, biggerCompare } = require('./util')
 
-console.log(createRandomArray());
+function selectSort(array) {
+  let minIndex;
+  for (let i = 0; i < array.length - 1; i++) {
+    minIndex = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (biggerCompare(array[minIndex], array[j])) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      swap(array, minIndex, i)
+    }
+  }
+  return array;
+}
+
+console.log(selectSort(createArray()));
